3eb3ca2915e259557bdb29929026c7c78eb940d1
Caio <caiomrr.rodrigues@gmail.com>
2025-05-14 22:42:46 -0300

    Calculadora de idade básica, primeiro projeto.

---
+++ Calculadora de idade.py
@@ -0,0 +1,27 @@
+import datetime
+ano_nascimento = int(input("Qual sua data de nascimento?:"))
+mes_nascimento = int(input("Qual seu mês de aniversario?:"))
+dia_nascimento = int(input("Qual o dia do seu aniversario?:"))
+
+ano_atual = datetime.datetime.now().year
+data_atual = datetime.datetime.now()
+
+if ano_nascimento > ano_atual:
+    print("ANO INVALIDO")
+else:
+    idade = ano_atual - ano_nascimento
+    print(f"Você tem {idade} anos.")
+
+if idade >= 60:
+    print("Você é idoso")
+elif idade >= 18:
+    print("Você é adulto")
+elif idade >= 12:
+    print("Você é adoslecente")
+else:
+    print("Você é criança")
+
+data_nascimento = datetime.datetime(ano_nascimento, mes_nascimento, dia_nascimento)
+dia_de_vida = (data_atual - data_nascimento).days
+
+print(f"Você tem aproximadamente {dia_de_vida} dias de vida: ")
