771e4272c6ff382d94b2e637d814e8fc67ce3191
Caio <caiomrr.rodrigues@gmail.com>
2025-05-20 21:12:56 -0300

    Calculadora de idade básica, usando datetime

--- Calculadora de idade.py
+++ Calculadora de idade.py
@@ -2,29 +2,28 @@ import datetime
 ano_nascimento = int(input("Qual sua data de nascimento?:"))
 mes_nascimento = int(input("Qual seu mês de aniversario?:"))
 dia_nascimento = int(input("Qual o dia do seu aniversario?:"))
-
 ano_atual = datetime.datetime.now().year
 data_atual = datetime.datetime.now()
-
+nome = input("Qual seu nome?:")
 if ano_nascimento > ano_atual:
     print("ANO INVALIDO")
 else:
     idade = ano_atual - ano_nascimento
-    print(f"Você tem {idade} anos.")
+    print(f"{nome} você tem {idade} anos.")
 
 if idade >= 60:
-    print("Você é idoso")
+    print(f"{nome} você é idoso")
 elif idade >= 18:
-    print("Você é adulto")
+    print(f"{nome} você é adulto")
 elif idade >= 12:
-    print("Você é adoslecente")
+    print(f"{nome} você é adoslecente")
 else:
-    print("Você é criança")
+    print(f"{nome} você é criança")
 
 data_nascimento = datetime.datetime(ano_nascimento, mes_nascimento, dia_nascimento)
 dia_de_vida = (data_atual - data_nascimento).days
 
-print(f"Você tem aproximadamente {dia_de_vida} dias de vida: ")
+print(f"{nome} você tem aproximadamente {dia_de_vida} dias de vida: ")
 
 hoje = datetime.date.today()
 ano_atual = hoje.year
@@ -38,4 +37,4 @@ else:
         proximo_aniversario = datetime.date(ano_atual + 1, mes_nascimento, dia_nascimento)
 
     dias_para_aniversario = (proximo_aniversario - hoje).days
-    print(f"Faltam {dias_para_aniversario} dias pro seu próximo aniversário!")
+    print(f"{nome} faltam {dias_para_aniversario} dias pro seu próximo aniversário!")
